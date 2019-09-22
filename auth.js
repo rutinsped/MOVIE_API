9296ea7b12a34c7b2ba2833dd81946544fd4ecce
rutinsped <rutinsped1@gmail.com>
2019-09-22 16:36:52 +0200

    Created auth.js file for jwt

---
+++ auth.js
@@ -0,0 +1,33 @@
+var jwtSecret = 'your_jwt_secret';
+var jwt = require('jsonwebtoken');
+const passport = require('passport');
+require('./passport'); 
+
+
+function generateJWTToken(user) {
+ return jwt.sign(user, jwtSecret, {
+   subject: user.Username, 
+   expiresIn: '7d', 
+   algorithm: 'HS256' 
+ });
+}
+
+module.exports = (router) => {
+ router.post('/login', (req, res) => {
+   passport.authenticate('local', { session : false}, (error, user, info) => {
+     if (error || !user) {
+       return res.status(400).json({
+         message: 'Something is not right',
+         user: user
+       });
+     }
+     req.login(user, { session: false }, (error) => {
+       if (error) {
+         res.send(error);
+       }
+       var token = generateJWTToken(user.toJSON());
+       return res.json({ user: user, token: token });
+     });
+   })(req, res);
+ });
+}
