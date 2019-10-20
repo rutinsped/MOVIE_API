9ad23c3b8f7a066a5885d32a17b1f9e694db6370
rutinsped <rutinsped1@gmail.com>
2019-10-20 12:12:49 +0200

    correcting

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/movies', function(req, res) {
   .catch(function(err) {
     console.error(err);
     res.status(500).send("Error: " + err);
-  })
+  });
 });
 
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), function(req, res) {
