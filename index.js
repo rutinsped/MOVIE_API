49478cacfae0fb485baab839ac77436b0b0baeed
rutinsped <rutinsped1@gmail.com>
2019-10-20 09:09:51 +0200

    Corrected code error

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.static("public"));
 
 // Get movies and details
 
-app.get('/movies'), function(req, res) {
+app.get('/movies', function(req, res) {
 
   Movies.find()
   .then(function(movies) {
@@ -51,7 +51,7 @@ app.get('/movies'), function(req, res) {
   .catch(function(err) {
     console.error(err);
     res.status(500).send("Error: " + err);
-  });
+  })
 });
 
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), function(req, res) {
