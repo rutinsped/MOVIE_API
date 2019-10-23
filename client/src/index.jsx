752bf705729aeddafe9d89348a54dc9cd8d32c9f
rutinsped <rutinsped1@gmail.com>
2019-10-23 21:11:48 +0200

    Not edited

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,16 +8,11 @@ import { MainView } from './components/main-view/main-view';
 import './index.scss';
 
 // Main component (will eventually use all the others)
-class Cinesider extends React.Component {
-  class MyFlixApplication extends React.Component {
-    render() {
-      return (
-        <div className="my-flix">
-          <div>Good morning</div>
-        </div>
-      );
-    }
+class MyFlixApplication extends React.Component {
+  render() {
+    return <MainView/>;
   }
+}
 
 // Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
