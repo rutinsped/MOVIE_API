7dd4dc1d6b0e8bb40f8f3b6f50b7f26f445176ac
rutinsped <rutinsped1@gmail.com>
2019-10-23 20:18:38 +0200

    Code error corrected

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,10 +9,15 @@ import './index.scss';
 
 // Main component (will eventually use all the others)
 class Cinesider extends React.Component {
-  render() {
-    return <MainView/>;
+  class MyFlixApplication extends React.Component {
+    render() {
+      return (
+        <div className="my-flix">
+          <div>Good morning</div>
+        </div>
+      );
+    }
   }
-}
 
 // Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
