ca36cb5d813f96aaf7d066976e50257ccae41d87
rutinsped <rutinsped1@gmail.com>
2019-10-28 17:53:36 +0100

    chnage homepage address

--- client/dist/index.js
+++ client/dist/index.js
@@ -34100,7 +34100,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50825" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53017" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
