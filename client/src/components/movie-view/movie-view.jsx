b76c9dcf756aac17deaa572d5e4054252bd65a9c
rutinsped <rutinsped1@gmail.com>
2019-11-03 08:09:46 +0100

    Test

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -17,20 +17,20 @@ export class MovieView extends React.Component {
     return (
        <div className="movie-view">
           <div className="movie-title">
-          <div className="label">Title</div>
+          <div className="label">Title:</div>
           <div className="value">{movie.Title}</div>
         </div>
         <div className="movie-description">
-          <div className="label">Description</div>
+          <div className="label">Description:</div>
           <div className="value">{movie.Description}</div>
         </div>
         <img className="movie-poster" src={movie.ImagePath} />
         <div className="movie-genre">
-          <div className="label">Genre</div>
+          <div className="label">Genre:</div>
           <div className="value">{movie.Genre.Name}</div>
         </div>
         <div className="movie-director">
-          <div className="label">Director</div>
+          <div className="label">Director:</div>
           <div className="value">{movie.Director.Name}</div>
         </div>
         <button
