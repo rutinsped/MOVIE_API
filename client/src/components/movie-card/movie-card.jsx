340d8bcde64dcd6719533c2271a94a0358146a98
rutinsped <rutinsped1@gmail.com>
2019-11-04 19:03:39 +0100

    Added prop types

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -16,8 +16,15 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.string.isRequired,
-    Director: PropTypes.string.isRequired
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired
+    })
   }).isRequired,
   onClick: PropTypes.func.isRequired
 };
