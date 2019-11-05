1b9517b5c4efba24314ab6f432ff2563ae9a2d50
rutinsped <rutinsped1@gmail.com>
2019-11-05 20:11:10 +0100

    test

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Grid from 'react-bootstrap/Grid';
+
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-      <Card style={{ width: '50%' }}>
+      <Card style={{ width: '20rem' }}>
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
