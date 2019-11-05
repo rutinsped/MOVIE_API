e70431b5c93158424e379fe0d71c1252c2452571
rutinsped <rutinsped1@gmail.com>
2019-11-05 20:08:28 +0100

    Test

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Grid from 'react-bootstrap/Grid';
 
 export class MovieCard extends React.Component {
   render() {
