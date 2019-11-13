dec0ba8161b7c077b9d8fe1e504ecb457ac521d9
rutinsped <rutinsped1@gmail.com>
2019-11-13 17:54:19 +0100

    test

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -39436,7 +39436,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54494" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64727" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
