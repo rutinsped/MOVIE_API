b0ff8f6a47775c3019973fbff91100d94b7e3664
rutinsped <rutinsped1@gmail.com>
2019-11-20 19:19:21 +0100

    Added registration route path

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -24,7 +24,7 @@ export class MainView extends React.Component {
     super();
 
     this.state = {
-      movies: null,
+      movies: [],
       user: null
     };
   }
@@ -91,15 +91,16 @@ export class MainView extends React.Component {
       <Router>
       <div className="main-view">
       <Route exact path="/" render={() => movies.map(m => <MovieCard key={m._id} movie={m}/>)}/>
-          <Route path="/movies/:movieId" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
-          <Route path="/directors/:name" render={({ match }) => {
-  if (!movies) return <div className="main-view"/>;
-  return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
-} />
-         <Route path="/genres/:name" render={({ match }) => {
-  if (!movies) return <div className="main-view"/>;
-  return <GenreView director={movies.find(m => m.Genre.Name === match.params.name).Genre}/>}
- } />
+      <Route path="/register" render={() => <RegistrationView />} />
+      <Route path="/movies/:movieId" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+      <Route path="/directors/:name" render={({ match }) => {
+      if (!movies) return <div className="main-view"/>;
+      return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
+    } />
+      <Route path="/genres/:name" render={({ match }) => {
+      if (!movies) return <div className="main-view"/>;
+      return <GenreView director={movies.find(m => m.Genre.Name === match.params.name).Genre}/>}
+    } />
          </div>
       </Router>
     );
