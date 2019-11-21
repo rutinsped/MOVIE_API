a3178db32bb8853c963b28578ea700e7ccabec9a
rutinsped <rutinsped1@gmail.com>
2019-11-21 17:23:06 +0100

    Render genre card

---
+++ client/src/components/genre-view/genre-view.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './movie-view.scss';
+import { Link } from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre } = this.props;
+
+    return (
+      <Card style={{ width: '20rem', height: '40rem', margin: '2rem' }}>
+        <Card.Body>
+          <Card.Title>{genre.Name}</Card.Title>
+          <Card.Text>{genre.Description}</Card.Text>
+          <Link to={`/`}></Link>
+          <Button className="primary" variant="link">Back</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+} 
