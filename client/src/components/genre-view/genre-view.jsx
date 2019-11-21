4c836ed6015def3ad1b2d257ebff4bd917919344
rutinsped <rutinsped1@gmail.com>
2019-11-21 17:25:50 +0100

    correcting error

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './movie-view.scss';
+import './genre-view.scss';
 import { Link } from "react-router-dom";
 
 import Container from 'react-bootstrap/Container';
