78142f0bfaa1c179037be3048e8493c905641ccd
rutinsped <rutinsped1@gmail.com>
2019-11-21 17:24:17 +0100

    Import card

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Spinner from 'react-bootstrap/Spinner';
+import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
